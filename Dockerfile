c40fb61d238c5788ba1da40514c05eb2e07a17bb
Srbino <pablosrbino@gmail.com>
2023-07-25 22:26:11 +0200

    prepare files

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Základní obraz
+FROM python:3.7
+
+# Nastavení pracovního adresáře v kontejneru
+WORKDIR /app
+
+# Kopírování souborů s námi potřebnými závislostmi do kontejneru
+COPY requirements.txt ./
+
+# Instalace závislostí
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Kopírování zdrojového kódu do kontejneru
+COPY . .
+
+# Spuštění Python skriptu
+CMD [ "python", "./app.py" ]
---
+++ calculator.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+        num1 = int(request.form.get('num1'))
+        num2 = int(request.form.get('num2'))
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            if num2 == 0:
+                return "Error: Division by zero"
+            result = num1 / num2
+        else:
+            return "Invalid operation"
+        return render_template('home.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
